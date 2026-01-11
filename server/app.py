ddcd242dabd787dfbe7e15eb83977bab0a8141d7
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2026-01-12 00:00:35 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Employee, Department
 
 # create a Flask application instance 
 app = Flask(__name__)
